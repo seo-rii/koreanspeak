4c6fac476df217c0386f9e5f1ee25f70e76123f7
Seo_Hyun <04seohyun@gmail.com>
2020-06-04 08:41:59 +0900

    Sync

--- main.py
+++ main.py
@@ -1,8 +1,57 @@
 import hgtk
+import rules.chap4 as chap4
 
 str = input()
 lst = list()
-for i in str:
-    lst.append(hgtk.letter.decompose(i))
+res = ""
 
-print(lst)
+for i in str:
+    if hgtk.checker.is_hangul(i):
+        lst.append([True, list(hgtk.letter.decompose(i))])
+    else:
+        lst.append([False, i])
+
+if len(lst) == 1:
+    a = lst[0][1][0]
+    b = lst[0][1][1]
+    c = lst[0][1][2]
+    (a, b, c) = chap4.rule9(a, b, c, None, None, None)
+    (a, b, c) = chap4.rule10(a, b, c, None, None, None)
+    (a, b, c) = chap4.rule11(a, b, c, None, None, None)
+    lst[0][1][0] = a
+    lst[0][1][1] = b
+    lst[0][1][2] = c
+
+for i in range(len(lst) - 1):
+    if lst[i][0]:
+        a = lst[i][1][0]
+        b = lst[i][1][1]
+        c = lst[i][1][2]
+        if lst[i + 1][0]:
+            p = lst[i + 1][1][0]
+            q = lst[i + 1][1][1]
+            r = lst[i + 1][1][2]
+        else:
+            p = False
+            q = False
+            r = False
+
+        (a, b, c, p, q, r) = chap4.rule9(a, b, c, p, q, r)
+        (a, b, c, p, q, r) = chap4.rule10(a, b, c, p, q, r)
+        (a, b, c, p, q, r) = chap4.rule11(a, b, c, p, q, r)
+        (a, b, c, p, q, r) = chap4.rule12(a, b, c, p, q, r)
+
+        lst[i][1][0] = a
+        lst[i][1][1] = b
+        lst[i][1][2] = c
+        lst[i + 1][1][0] = p
+        lst[i + 1][1][1] = q
+        lst[i + 1][1][2] = r
+
+for i in lst:
+    if i[0]:
+        res += hgtk.letter.compose(i[1][0], i[1][1], i[1][2])
+    else:
+        res += i[1]
+
+print(res)
---
+++ rules/chap4.py
@@ -0,0 +1,96 @@
+def rule9(a, b, c, p, q, r):
+    if c == 'ㅋ' or c == 'ㄲ':
+        c = 'ㄱ'
+    if c == 'ㅅ' or c == 'ㅆ' or c == 'ㅈ' or c == 'ㅊ' or c == 'ㅌ':
+        c = 'ㄷ'
+    if c == 'ㅍ':
+        c = 'ㅂ'
+    return (a, b, c, p, q, r)
+
+
+def rule10(a, b, c, p, q, r):
+    if c == 'ㄳ':
+        c = 'ㄱ'
+    if c == 'ㄵ':
+        c = 'ㄴ'
+    if c == 'ㄼ' or c == 'ㄽ' or c == 'ㄾ':
+        if c == 'ㄼ' and (not (p == 'ㅇ') and p):
+            c = 'ㅂ'
+        else:
+            c = 'ㄹ'
+    if c == 'ㅄ':
+        c = 'ㅂ'
+    return (a, b, c, p, q, r)
+
+
+def rule11(a, b, c, p, q, r):
+    if c == 'ㄺ':
+        if p == 'ㄱ':
+            c = 'ㄹ'
+        else:
+            c = 'ㄱ'
+    if c == 'ㄻ':
+        c = 'ㅁ'
+    if c == 'ㄿ':
+        c = 'ㅂ'
+    return (a, b, c, p, q, r)
+
+
+def rule12(a, b, c, p, q, r):
+    if c == 'ㅎ' or c == 'ㅀ' or c == 'ㄶ':
+        if p == 'ㄱ':
+            p = 'ㅋ'
+            if c == 'ㅀ':
+                c = 'ㄹ'
+            elif c == 'ㄶ':
+                c = 'ㄴ'
+            else:
+                c = ''
+        elif p == 'ㄷ':
+            p = 'ㅌ'
+            if c == 'ㅀ':
+                c = 'ㄹ'
+            elif c == 'ㄶ':
+                c = 'ㄴ'
+            else:
+                c = ''
+        elif p == 'ㅈ':
+            p = 'ㅊ'
+            if c == 'ㅀ':
+                c = 'ㄹ'
+            elif c == 'ㄶ':
+                c = 'ㄴ'
+            else:
+                c = ''
+        elif p == 'ㅅ':
+            p = 'ㅆ'
+            if c == 'ㅀ':
+                c = 'ㄹ'
+            elif c == 'ㄶ':
+                c = 'ㄴ'
+            else:
+                c = ''
+        elif p == 'ㄴ':
+            if c == 'ㅀ':
+                c = 'ㄹ'
+            elif c == 'ㄶ':
+                c = 'ㄴ'
+            else:
+                c = 'ㄴ'
+        elif p == 'ㅇ':
+            if c == 'ㅀ':
+                p = 'ㄹ'
+            elif c == 'ㄶ':
+                p = 'ㄴ'
+            c = ''
+    if p == 'ㅎ':
+        if c == 'ㄱ':
+            p = 'ㅋ'
+            c = ''
+        elif c == 'ㄷ':
+            p = 'ㅌ'
+            c = ''
+        elif c == 'ㅈ':
+            p = 'ㅊ'
+            c = ''
+    return (a, b, c, p, q, r)
